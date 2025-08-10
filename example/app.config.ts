6dec8adc84b7b5a34221dd2561d8ee51f4d5a3bf
Mateus Marinho <mateus.santos.840@hotmail.com>
2025-08-10 03:17:00 -0300

    refactor: using expo

---
+++ example/app.config.ts
@@ -0,0 +1,13 @@
+import { ConfigContext, ExpoConfig } from 'expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'example-bottom-sheet-app',
+  slug: 'example-bottom-sheet-app',
+  android: {
+    package: 'example.bottom.sheet.app',
+  },
+  ios: {
+    bundleIdentifier: 'example.bottom.sheet.app',
+  },
+});
